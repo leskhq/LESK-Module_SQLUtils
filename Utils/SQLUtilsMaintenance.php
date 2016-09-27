dc7117fe95efdb746b90c63f9db33161e8668afd
Sebastien Routier <sroutier@gmail.com>
2016-09-27 11:32:09 -0400

    Rename project from l51esk to lesk.

--- Utils/SQLUtilsMaintenance.php
+++ Utils/SQLUtilsMaintenance.php
@@ -1,6 +1,6 @@
 <?php namespace App\Modules\SQLUtils\Utils;
 
-use Sroutier\L51ESKModules\Contracts\ModuleMaintenanceInterface;
+use Sroutier\LESKModules\Contracts\ModuleMaintenanceInterface;
 use DB;
 
 class SQLUtilsMaintenance implements ModuleMaintenanceInterface {
